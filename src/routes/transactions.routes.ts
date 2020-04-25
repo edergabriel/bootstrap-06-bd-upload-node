a955ebe46d59c44ded619129ef85cbb0e38a89df
Eder Gabriel <eder1602@outlook.com>
2020-04-24 23:09:37 -0300

    Finalização desafio 06

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
 import { getCustomRepository } from 'typeorm';
+import multer from "multer";
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+import uploadConfig from '../config/upload';
+
+const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
 
@@ -43,8 +48,13 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.status(204).send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
+transactionsRouter.post('/import', upload.single('file'), async (request, response) => {
   // TODO
+  const ImportTransactions = new ImportTransactionsService();
+
+  const transactions = await ImportTransactions.execute(request.file.path);
+
+  return response.json(transactions);
 });
 
 export default transactionsRouter;
